3e45b800ed539d75ae5015c109f5ce76568dbada
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-04-04 15:03:29 -1000

    got the bg going

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Calendar2CheckFill, FileEarmarkTextFill, PeopleFill } from 'react-bootstrap-icons';
+import { Container, Col, Row } from 'react-bootstrap';
 
-/* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={12} className="mb-4">
+    <Row className="justify-content-start align-items-center py-4 mb-4 extra-space">
+      <Col xs={12} md={6} className="mb-4">
         <h1>Welcome to our platform!</h1>
-        {/* eslint-disable-next-line max-len */}
-        <p>Here, you’ll find a vibrant community of learners dedicated to academic excellence. Our landing page showcases images of enthusiastic individuals engaged in studying, setting the tone for an interactive and collaborative learning experience. Get ready to explore our application, designed to enhance your educational journey.</p>
+        <p className="text-left">Here, you’ll find a vibrant community of learners dedicated to academic excellence.
+          Our landing page showcases images of enthusiastic individuals engaged in studying,
+          setting the tone for an interactive and collaborative learning experience. Get ready to explore our application, designed to enhance your educational journey.
+        </p>
       </Col>
-      <Col xs={4}>
-        <FileEarmarkTextFill size={100} />
-        <h1>Contact Details</h1>
-        <h5>For each contact, you can save their name, address, and phone number.</h5>
-      </Col>
-
-      <Col xs={4}>
-        <Calendar2CheckFill size={100} />
-        <h1>Timestamped Notes</h1>
-        <h5>Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</h5>
-      </Col>
-
-      <Col xs={4}>
-        <PeopleFill size={100} />
-        <h1>Multiple Users</h1>
-        <h5>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h5>
-      </Col>
-    </Row>
-    <Row className="justify-content-center">
-      <Col xs={8} className="text-center" />
     </Row>
   </Container>
 );
