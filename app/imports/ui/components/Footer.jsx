c38ff90e1f59efb886a481563a1b573394f40139
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-04-07 21:44:08 -1000

    landing page DONEEEE

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,20 +3,14 @@ import { Col, Container } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-dark">
+  <footer className="mt-auto py-1 bg-dark"> {/* Reduced padding */}
     <Container>
       <Col className="text-center">
-        Department of Information and Computer Sciences
+        Created and Maintained by: Roger Tulonghari, Grace Madson, Hazelle Limos, Robert Maddox, and Hau’oli O’Brien
         {' '}
         <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
+        <a href="https://manoa-mentoring.github.io/">
+          More about our Project here (click me!)
         </a>
       </Col>
     </Container>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,15 +4,19 @@ import { Container, Col, Row } from 'react-bootstrap';
 const Landing = () => (
   <Container id="landing-page" className="py-3">
     <Row className="justify-content-start align-items-center py-4 mb-4 extra-space">
-      <Col xs={12} md={6} className="mb-4">
-        <h1>Welcome to our platform!</h1>
-        <p className="text-left">Here, you’ll find a vibrant community of learners dedicated to academic excellence.
-          Our landing page showcases images of enthusiastic individuals engaged in studying,
-          setting the tone for an interactive and collaborative learning experience. Get ready to explore our application, designed to enhance your educational journey.
+      <Col xs={10} md={4} className="mb-4">
+        <h1 style={{ color: 'var(--matr-navbar-text-color)' }}>Welcome to Manoa Mentoring!</h1>
+        <p className="text-lg-left" style={{ color: 'var(--matr-navbar-text-color)' }}>Our application, Manoa Mentoring, aims to bring together students
+          and mentors. Students can match themselves with other students
+          who are facing similar issues, or they can choose a mentor
+          who specializes in a subject or course they are taking.
+          Through Manoa Mentoring, our goal is to create a space
+          where students can not only receive the support they need,
+          but also foster connections through mentorship and potential
+          friendships.
         </p>
       </Col>
     </Row>
   </Container>
 );
-
 export default Landing;
