457625cb4842a1ad99401ab839b556d9fff7c3c8
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-04-18 00:15:51 -1000

    fixed a little, need to get rid of green nav

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,10 +3,14 @@ import { Container, Col, Row } from 'react-bootstrap';
 
 const Landing = () => (
   <Container id="landing-page" className="py-3">
-    <Row className="justify-content-start align-items-center py-4 mb-4 extra-space">
-      <Col xs={10} md={4} className="mb-4">
-        <h1 style={{ color: 'var(--matr-navbar-text-color)' }}>Welcome to Manoa Mentoring!</h1>
-        <p className="text-lg-left" style={{ color: 'var(--matr-navbar-text-color)' }}>Our application, Manoa Mentoring, aims to bring together students
+    <Row className="justify-content-center align-items-center py-5 mb-4 extra-space">
+      <Col xs={9} md={4} className="mb-4 text-center" style={{ marginRight: '50px' }}>
+        <p style={{
+          color: 'var(--matr-navbar-text-color)',
+          textShadow: '0 0 10px black', /* Add black outline */
+        }}
+        >
+          Our application, Manoa Mentoring, aims to bring together students
           and mentors. Students can match themselves with other students
           who are facing similar issues, or they can choose a mentor
           who specializes in a subject or course they are taking.
