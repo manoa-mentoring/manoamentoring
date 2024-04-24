f9827c588c9b691afe7524ec947514fb0b403194
grace-madson <gmadson@hawaii.edu>
2024-04-23 14:45:39 -1000

    Styled the calendar

--- app/imports/ui/components/Calendar.jsx
+++ app/imports/ui/components/Calendar.jsx
@@ -8,9 +8,14 @@ const Calendar = () => (
     plugins={[dayGridPlugin]}
     initialView="dayGridMonth"
     events={[
-      { title: 'Event 1', date: '2024-04-01' },
-      { title: 'Event 2', date: '2024-04-15' },
+      { title: 'ICS 314: WOD #7 Workshop', date: '2024-04-01T10:00:00', end: '2024-04-01T12:00:00' },
+      { title: 'ICS 369: Maya Animation Review', date: '2024-04-15T14:00:00', end: '2024-04-15T16:00:00' },
     ]}
+    eventTimeFormat={{
+      hour: 'numeric',
+      minute: '2-digit',
+      meridiem: false,
+    }}
   />
 );
 
--- app/imports/ui/pages/CalendarPage.jsx
+++ app/imports/ui/pages/CalendarPage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Calendar from '../components/Calendar';
 
 const CalendarPage = () => (
-  <div className="App">
-    <h1 style={{ color: 'var(--matr-navbar-text-color)' }}>Scheduled Study Sessions</h1>
+  <div className="App" style={{ backgroundColor: 'white' }}>
+    <h1>Scheduled Study Sessions</h1>
     <Calendar />
   </div>
 );
