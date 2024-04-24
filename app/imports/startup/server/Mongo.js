64cebaabb481ea62fe0ccd482b58efa4e51c8044
rogerkt2 <rkt2@hawaii.edu>
2024-04-23 15:59:17 -1000

    Created default profiles for user and admin

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Contacts } from '../../api/contact/Contacts';
 import { StudySessions } from '../../api/studysession/StudySession';
+import { Profiles } from '../../api/profile/Profiles';
 
 /* eslint-disable no-console */
 
@@ -25,5 +26,17 @@ if (StudySessions.collection.find().count() === 0) {
   if (Meteor.settings.defaultStudySessions) {
     console.log('Creating default study sessions.');
     Meteor.settings.defaultStudySessions.forEach(studySession => addStudySession(studySession));
+  }
+}
+
+const addProfile = (profile) => {
+  console.log(`  Creating profile for: ${profile.firstName} (${profile.owner})`);
+  Profiles.collection.insert(profile);
+};
+
+if (Profiles.collection.find().count() === 0) {
+  if (Meteor.settings.defaultProfiles) {
+    console.log('Creating default profile.');
+    Meteor.settings.defaultProfiles.forEach(profile => addProfile(profile));
   }
 }
