5ba0a733c9801ad21bb4eaed3a9bcd5ffafbec43
Robert Maddox <39988090+robertmaddoxhi@users.noreply.github.com>
2024-04-23 22:19:46 -1000

    Fixed small merging issues

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -53,15 +53,14 @@ class NavBar {
     await testController.click('#login-dropdown-sign-up');
   }
 
-
   async gotoListContactsAdmin(testController) {
     await testController.click('#list-stuff-admin-nav');
   }
-  
+
   async gotoCreateSessionPage(testController) {
     await testController.click('#create-session-nav');
   }
-  
+
   async gotoEditSessionPage(testController) {
     await testController.click('#edit-session-nav');
   }
